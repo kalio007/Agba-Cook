5677b73b1b133122b546e4880aa7120fd28a8fac
Kalio007 <Kalioprince1@gmail.com>
2022-12-13 20:40:47 +0100

    added the Intro

--- resturant/src/App.js
+++ resturant/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from './Components/NavBar/Navbar';
 import SubHeading from './Components/SubHeading/SubHeading';
+import AboutUs from './Components/AboutUs/AboutUs';
+import SpecialMenu from './Components/SpecialMenu/SpecialMenu';
+import Chef from './Components/Chef/Chef';
+import Intro from './Components/Intro/Intro';
 
 
 
@@ -13,6 +17,9 @@ function App() {
     <div>
      <Navbar/>
      <Header/>
+     <AboutUs/>
+     <SpecialMenu/>
+     <Intro/>
     </div>
   );
 }
